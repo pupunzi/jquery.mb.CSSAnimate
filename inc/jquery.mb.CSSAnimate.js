3b4ad8ed99240b13e3b998635dcb8eacaf883224
pupunzi <matteo@open-lab.com>
2014-02-18 01:33:29 +0100

    various fix

--- inc/jquery.mb.CSSAnimate.js
+++ inc/jquery.mb.CSSAnimate.js
@@ -14,7 +14,7 @@
  *  http://www.opensource.org/licenses/mit-license.php
  *  http://www.gnu.org/licenses/gpl.html
  *
- *  last modified: 27/01/14 20.02
+ *  last modified: 18/02/14 1.31
  *  *****************************************************************************
  */
 
@@ -51,27 +51,43 @@ if (!jQuery.browser) {
 jQuery.fn.CSSAnimate = function (opt, duration, delay, ease, callback) {
 	return this.each(function () {
 
-		var el = jQuery(this);
+		var el = this;
+		var $el = jQuery(this);
+		el.id = el.id || "CSSA_" + new Date().getTime();
 
-		this.id = this.id || "CSSA_" + new Date().getTime();
+		var event = event || {type:"noEvent"};
 
-		if (el.length === 0 || !opt) {
+		if(el.CSSAIsRunning && el.eventType == event.type){
+			el.CSSqueue = function(){
+				$el.CSSAnimate(opt, duration, delay, ease, callback);
+			}
 			return;
 		}
+
+		el.CSSqueue=null;
+		el.eventType = event.type;
+
+
+		if ($el.length === 0 || !opt) {
+			return;
+		}
+
+		el.CSSAIsRunning = true;
+		
 
 		if (typeof duration == "function") {
 			callback = duration;
 			duration = jQuery.fx.speeds["_default"];
 		}
+
 		if (typeof delay == "function") {
 			callback = delay;
-			delay = 0
+			delay = 0;
 		}
 		if (typeof ease == "function") {
 			callback = ease;
 			ease = "cubic-bezier(0.65,0.03,0.36,0.72)";
 		}
-
 
 		if (typeof duration == "string") {
 			for (var d in jQuery.fx.speeds) {
@@ -79,13 +95,13 @@ jQuery.fn.CSSAnimate = function (opt, duration, delay, ease, callback) {
 					duration = jQuery.fx.speeds[d];
 					break;
 				} else {
-					duration = null;
+					duration = jQuery.fx.speeds["_default"];
 				}
 			}
 		}
 
-		//http://cssglue.com/cubic
-		//  ease  |  linear | ease-in | ease-out | ease-in-out  |  cubic-bezier(<number>, <number>,  <number>,  <number>)
+		if(!duration)
+			duration = jQuery.fx.speeds["_default"];
 
 		if (!jQuery.support.transition) {
 
@@ -107,9 +123,43 @@ jQuery.fn.CSSAnimate = function (opt, duration, delay, ease, callback) {
 			if (!callback || typeof callback === "string")
 				callback = "linear";
 
-			el.animate(opt, duration, callback);
+			$el.animate(opt, duration, callback);
 			return;
 		}
+
+		var cssEase = {
+			'default':       'ease',
+			'in':             'ease-in',
+			'out':            'ease-out',
+			'in-out':         'ease-in-out',
+			'snap':           'cubic-bezier(0,1,.5,1)',
+			'easeOutCubic':   'cubic-bezier(.215,.61,.355,1)',
+			'easeInOutCubic': 'cubic-bezier(.645,.045,.355,1)',
+			'easeInCirc':     'cubic-bezier(.6,.04,.98,.335)',
+			'easeOutCirc':    'cubic-bezier(.075,.82,.165,1)',
+			'easeInOutCirc':  'cubic-bezier(.785,.135,.15,.86)',
+			'easeInExpo':     'cubic-bezier(.95,.05,.795,.035)',
+			'easeOutExpo':    'cubic-bezier(.19,1,.22,1)',
+			'easeInOutExpo':  'cubic-bezier(1,0,0,1)',
+			'easeInQuad':     'cubic-bezier(.55,.085,.68,.53)',
+			'easeOutQuad':    'cubic-bezier(.25,.46,.45,.94)',
+			'easeInOutQuad':  'cubic-bezier(.455,.03,.515,.955)',
+			'easeInQuart':    'cubic-bezier(.895,.03,.685,.22)',
+			'easeOutQuart':   'cubic-bezier(.165,.84,.44,1)',
+			'easeInOutQuart': 'cubic-bezier(.77,0,.175,1)',
+			'easeInQuint':    'cubic-bezier(.755,.05,.855,.06)',
+			'easeOutQuint':   'cubic-bezier(.23,1,.32,1)',
+			'easeInOutQuint': 'cubic-bezier(.86,0,.07,1)',
+			'easeInSine':     'cubic-bezier(.47,0,.745,.715)',
+			'easeOutSine':    'cubic-bezier(.39,.575,.565,1)',
+			'easeInOutSine':  'cubic-bezier(.445,.05,.55,.95)',
+			'easeInBack':     'cubic-bezier(.6,-.28,.735,.045)',
+			'easeOutBack':    'cubic-bezier(.175, .885,.32,1.275)',
+			'easeInOutBack':  'cubic-bezier(.68,-.55,.265,1.55)'
+		};
+
+		if (cssEase[ease])
+			ease = cssEase[ease];
 
 		var sfx = "";
 		var transitionEnd = "transitionEnd";
@@ -128,8 +178,11 @@ jQuery.fn.CSSAnimate = function (opt, duration, delay, ease, callback) {
 		}
 
 		var prop = [];
+
 		for (var o in opt) {
+
 			var key = o;
+
 			if (key === "transform") {
 				key = sfx + "transform";
 				opt[key] = opt[o];
@@ -140,53 +193,193 @@ jQuery.fn.CSSAnimate = function (opt, duration, delay, ease, callback) {
 				opt[key] = opt[o];
 				delete opt[o];
 			}
-			if (key === "transform-origin") {
+			if (key === "transform-origin" || key === "origin") {
 				key = sfx + "transform-origin";
 				opt[key] = opt[o];
 				delete opt[o];
 			}
-			prop.push(key);
+
+			/**
+			 * Translate
+			 * */
+
+			if (key === "x") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" translateX("+setUnit(opt[o],"px")+")");
+				delete opt[o];
+			}
+
+			if (key === "y") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" translateY("+setUnit(opt[o],"px")+")");
+				delete opt[o];
+			}
+
+			if (key === "z") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" translateZ("+setUnit(opt[o],"px")+")");
+				delete opt[o];
+			}
+
+			/**
+			 * Rotate
+			 * */
+			if (key === "rotate") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" rotate("+setUnit(opt[o],"deg")+")");
+				delete opt[o];
+			}
+
+			if (key === "rotateX") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" rotateX("+setUnit(opt[o],"deg")+")");
+				delete opt[o];
+			}
+
+			if (key === "rotateY") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" rotateY("+setUnit(opt[o],"deg")+")");
+				delete opt[o];
+			}
+
+			if (key === "rotateZ") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" rotateZ("+setUnit(opt[o],"deg")+")");
+				delete opt[o];
+			}
+
+			/**
+			 * Scale
+			 * */
+			if (key === "scale") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" scale("+setUnit(opt[o],"")+")");
+				delete opt[o];
+			}
+
+			if (key === "scaleX") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" scaleX("+setUnit(opt[o],"")+")");
+				delete opt[o];
+			}
+
+			if (key === "scaleY") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" scaleY("+setUnit(opt[o],"")+")");
+				delete opt[o];
+			}
+
+			if (key === "scaleZ") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" scaleZ("+setUnit(opt[o],"")+")");
+				delete opt[o];
+			}
+
+			/**
+			 * Skew
+			 * */
+
+			if (key === "skew") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" skew("+setUnit(opt[o],"deg")+")");
+				delete opt[o];
+			}
+
+			if (key === "skewX") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" skewX("+setUnit(opt[o],"deg")+")");
+				delete opt[o];
+			}
+
+			if (key === "skewY") {
+				key = sfx + "transform";
+				opt[key] = opt[key] || "";
+				opt[key]+= (" skewY("+setUnit(opt[o],"deg")+")");
+				delete opt[o];
+			}
+
+			if(prop.indexOf(key)<0)
+				prop.push(key);
 
 		}
+
+		function setUnit(i, units) {
+			if ((typeof i === "string") && (!i.match(/^[\-0-9\.]+$/))) {
+				return i;
+			} else {
+				return "" + i + units;
+			}
+		}
+
 		var properties = prop.join(",");
 
-		var endTransition = function (e) {
-			el.off(transitionEnd+"."+el.get(0).id);
-			clearTimeout(el.get(0).timeout);
-			el.css(sfx + "transition", "");
+
+		var endTransition = function () {
+			$el.off(transitionEnd+"."+el.id);
+			clearTimeout(el.timeout);
+			$el.origCss(sfx + "transition", "");
 			if (typeof callback == "function") {
 				el.called = true;
-				callback(el);
+				callback($el);
+			}
+
+			el.CSSAIsRunning = false;
+			if(typeof el.CSSqueue == "function"){
+				el.CSSqueue();
+				el.CSSqueue=null;
 			}
 		};
 
 		var css ={};
 		$.extend(css,opt);
+
 		css[sfx + "transition-property"] = properties;
 		css[sfx + "transition-duration"] = duration + "ms";
 		css[sfx + "transition-delay"] = delay + "ms";
+		css[sfx + "transition-style"] = "preserve-3d";
 		css[sfx + "transition-timing-function"] = ease;
-		css[sfx + "backface-visibility"] = "hidden";
+//		css[sfx + "backface-visibility"] = "hidden";
 
 		setTimeout(function(){
-			el.css(css);
-			el.one(transitionEnd+"."+el.get(0).id, endTransition);
+			$el.one(transitionEnd+"."+el.id, endTransition);
+			$el.origCss(css);
 		},1);
 
 		//if there's no transition than call the callback anyway
-		el.get(0).timeout = setTimeout(function () {
-			if (el.called || !callback) {
-				el.called = false;
+		el.timeout = setTimeout(function () {
+			if ($el.called || !callback) {
+				$el.called = false;
+				el.CSSAIsRunning = false;
 				return;
 			}
+			$el.origCss(sfx + "transition", "");
+			callback($el);
 
-			el.css(sfx + "transition", "");
-			callback(el);
+			el.CSSAIsRunning = false;
+			if(typeof el.CSSqueue == "function"){
+				el.CSSqueue();
+				el.CSSqueue=null;
+			}
 
-		}, duration+ delay + 100);
+
+		}, duration + delay + 100);
 
 	})
 };
+
 // jQuery.support.transition
 // to verify that CSS3 transition is supported (or any of its browser-specific implementations)
 jQuery.support.transition = (function () {
@@ -194,3 +387,12 @@ jQuery.support.transition = (function () {
 	var thisStyle = thisBody.style;
 	return thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined;
 })();
+
+/* If transitions are supported overwrite both "animate" and "css" methods*/
+if (jQuery.support.transition) {
+	$.fn.origCss = $.fn.css;
+	$.fn.css = function(opt){
+		return this.CSSAnimate(opt,1,0,null,null);
+	};
+	$.fn.animate = $.fn.CSSAnimate;
+}
